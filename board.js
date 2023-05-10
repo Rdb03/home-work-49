673ba77fa0654eb9ccff883ec2e81110d451dd0b
Ramil Dzhafarov <rdb03@mail.ru>
2023-05-10 20:49:05 +0500

    Добавил структуру проета

---
+++ board.js
@@ -0,0 +1,16 @@
+"use strict";
+const boardSize = 8;
+const whiteSquare = '██';
+const blackSquare = '  ';
+for (let i = 0; i < boardSize; i++) {
+    let row = '';
+    for (let j = 0; j < boardSize; j++) {
+        if ((i + j) % 2 === 0) {
+            row += whiteSquare;
+        }
+        else {
+            row += blackSquare;
+        }
+    }
+    console.log(row);
+}
